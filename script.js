c7e43c9aa8549739d39d8b0590c9421b62dd3b37
MeoMapOnline <muncute0377392767@gmail.com>
2025-08-10 17:00:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function toggleMenu() {
+    let menu = document.getElementById("sideMenu");
+    if (menu.style.width === "250px") {
+        menu.style.width = "0";
+    } else {
+        menu.style.width = "250px";
+    }
+}
